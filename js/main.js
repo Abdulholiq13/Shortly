fa02d79735fd362d357cfee573e2de0ace49de0e
Abdulholiq13 <abdulholiq571@gmail.com>
2023-07-14 14:47:15 +0500

    Add url copy button click handler. Wrap form submit handler in if statement

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
+const elUrlShortener = document.querySelector(".url-shortener");
 const elUrlShortenerForm = document.querySelector(".js-url-shortener-form");
 const elUrlShortenerResults = document.querySelector(".url-shortener__results");
 
-elUrlShortenerForm.addEventListener('submit', function (evt) {
-  evt.preventDefault();
+if (elUrlShortenerForm) {
+  elUrlShortenerForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+  
+    elUrlShortenerResults.classList.add('url-shortener__results--open')
+  });
+}
 
-  elUrlShortenerResults.classList.add('url-shortener__results--open')
-});
+if (elUrlShortener) {
+  elUrlShortener.addEventListener('click', function (evt) {
+    if (evt.target.matches(".js-copy-short-link-button")) {
+      // Change button text
+      evt.target.textContent = "Copied!";
+
+      // Change butto bgcolor
+      evt.target.classList.add('url-shortener__cop-button--copied');
+
+      // Reset button text and class after 1 second
+      setTimeout (function () {
+        evt.target.textContent = 'Copy';
+        evt.target.remove('url-shortener__cop-button--copied')
+      }, 1000);
+    }
+  });
+}
